e6f364a4487a0f42f9f5201f418cdc35ba6f78dd
Ondrej Metelka <ometelka@redhat.com>
2026-09-18 09:54:32 +0200

    OLS-0000 remove Konflux marker comment

--- test/agent/main.go
+++ test/agent/main.go
@@ -14,7 +14,6 @@
 //
 // Build:  make -C test/agent docker-build
 // Image:  quay.io/openshift-lightspeed/ols-qe:lightspeed-mock-agent1
-// Konflux health-check marker; no runtime behavior change.
 package main
 
 import (
